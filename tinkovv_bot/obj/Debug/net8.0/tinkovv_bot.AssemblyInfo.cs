b7b4894b94dc9f972051b458ae2f234643e867fe
Gomick <gomickman@mail.ru>
2024-02-03 20:34:55 +0400

    какая то фигня

--- tinkovv_bot/obj/Debug/net8.0/tinkovv_bot.AssemblyInfo.cs
+++ tinkovv_bot/obj/Debug/net8.0/tinkovv_bot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tinkovv_bot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d22586f3bc0864ce106b155e7f01350b33298c65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("tinkovv_bot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tinkovv_bot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
