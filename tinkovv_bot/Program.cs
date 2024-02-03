b9bb0e8496ad882f72e3f04885066f9fe435f662
Yoonton <mak-sim-2005@mail.ru>
2024-02-03 13:07:29 +0400

    ответ на начать
    полу рабочее обращение

--- tinkovv_bot/Program.cs
+++ tinkovv_bot/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace tinkovv_bot
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+namespace tinkovv_bot
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var client = new TelegramBotClient("6878733229:AAGOk_4FkCvRmDSY1anMdyFSJfaK9FLkOrU");
+            client.StartReceiving(Update, Error);
+            Console.ReadLine();
+        }
+
+        private static Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        async static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
+        {
+            var message = update.Message;
+            if(message.Text != null)
+            {
+                if(message.Text.ToLower().Contains("начать"))
+                {
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "аааааа че я делаю");
+                    return;
+                }
+
+            }
+            throw new NotImplementedException();
         }
     }
 }
--- tinkovv_bot/obj/Debug/net8.0/tinkovv_bot.AssemblyInfo.cs
+++ tinkovv_bot/obj/Debug/net8.0/tinkovv_bot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tinkovv_bot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36373358d4ee131ee8990984c6c6898e6fbe5035")]
 [assembly: System.Reflection.AssemblyProductAttribute("tinkovv_bot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tinkovv_bot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
