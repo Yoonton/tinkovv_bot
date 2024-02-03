359dd840dfd70616f097f7cb0a91176afa028751
Gomick <gomickman@mail.ru>
2024-02-03 15:11:21 +0400

    asd

--- tinkovv_bot/Converter.cs
+++ tinkovv_bot/Converter.cs
@@ -1,33 +1,13 @@
-﻿using System.Globalization;
-
-public static class Converter
+﻿public static class Converter
 {
     public static int StringToInt(string input)
     {
-        int result;
-        if (int.TryParse(input, out result))
-        {
-            return result;
-        }
-        else
-        {
-            throw new ArgumentException("Невозможно преобразовать в число");
-        }
+        int result = int.Parse(input);
+        return result;
     }
-
     public static DateTime StringToDate(string input)
     {
-
-        string cleanedInput = input.Replace(":", "").Replace(",", ".");
-
-        DateTime result;
-        if (DateTime.TryParse(cleanedInput, CultureInfo.InvariantCulture, DateTimeStyles.None, out result))
-        {
-            return result;
-        }
-        else
-        {
-            throw new ArgumentException("Невозможно преобразовать в дату");
-        }
+        DateTime result = DateTime.Parse(input);
+        return result;
     }
 }
