a7c844f028434b3dc25a14ab1b324d6bf7e83eae
Gomick <gomickman@mail.ru>
2024-02-03 13:48:14 +0400

    готовые структуры

--- tinkovv_bot/Cost.cs
+++ tinkovv_bot/Cost.cs
@@ -2,12 +2,42 @@
 {
     public class Cost
     {
+        List<Expenses> expenses = new List<Expenses>();
         private struct Expenses
         {
             private DateTime date;
             private string category;
             private int sum;
-            int asd;
+            public DateTime Date
+            {
+                get { return date; }
+                set { date = value; }
+            }
+            public string Category
+            {
+                get { return category; }
+                set { category = value; }
+            }
+            public int Sum
+            {
+                get { return sum; }
+                set { sum = value; }
+            }
+        }
+        private struct Income
+        {
+            private DateTime date;
+            private int sum;
+            public DateTime Date
+            {
+                get { return date; }
+                set { date = value; }
+            }
+            public int Sum
+            {
+                get { return sum; }
+                set { sum = value; }
+            }
         }
     }
 }
